85e8eb19e0ed584949088737bf56577d83579265
mikenereson <mikenereson@fa5e1c15-7635-0410-8054-0fc223291177>
2007-10-06 19:58:15 +0000

    docs

--- src/main/java/com/seventytwomiles/architecturerules/services/CyclicRedundencyService.java
+++ src/main/java/com/seventytwomiles/architecturerules/services/CyclicRedundencyService.java
@@ -49,7 +49,7 @@ public class CyclicRedundencyService extends AbstractArchitecturalRules {
 
     /**
      * <p>Check all the packages in all of the source directories and search for
-     * any cyclic redundency. </p>
+     * any cyclic redundenc/p>
      *
      * @throws CyclicRedundancyException when cyclic redundency is found
      */
