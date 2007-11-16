55686d10846a683725e922691bd29d142d389a4f
mikenereson <mikenereson@fa5e1c15-7635-0410-8054-0fc223291177>
2007-11-16 19:24:24 +0000

    updated version

--- src/test/java/com/seventytwomiles/architecturerules/exceptions/SourceNotFoundExceptionTest.java
+++ src/test/java/com/seventytwomiles/architecturerules/exceptions/SourceNotFoundExceptionTest.java
@@ -101,9 +101,6 @@ public class SourceNotFoundExceptionTest extends TestCase {
         assertTrue(message.indexOf("parent-pom/target/classes") > -1);
         assertTrue(message.indexOf("web/target/classes") > -1);
 
-        /* TODO: why tf does the following assert fail? */
-        // assertTrue(message.contains("unable to find any source files in given source directories [web/target/classes, core/target/classes, parent-pom/target/classes, util/target/classes]"));
-
         assertEquals(null, cause);
     }
 }
