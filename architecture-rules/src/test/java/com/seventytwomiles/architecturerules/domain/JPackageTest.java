b16c928932b283f60c76e0f0eb9e72941d393846
mikenereson <mikenereson@fa5e1c15-7635-0410-8054-0fc223291177>
2008-06-20 11:52:34 +0000

    additional wildcard matching tests wildcards are supported as internal wildcards or terminating wildcards with .* package or ..* subpackage

--- architecture-rules/src/test/java/com/seventytwomiles/architecturerules/domain/JPackageTest.java
+++ architecture-rules/src/test/java/com/seventytwomiles/architecturerules/domain/JPackageTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2007 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *         http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * For more information visit
  *         http://72miles.com and
  *         http://architecturerules.googlecode.com/
@@ -18,6 +18,9 @@ package com.seventytwomiles.architecturerules.domain;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 
@@ -36,7 +39,7 @@ public class JPackageTest extends TestCase {
     }
 
 
-    public JPackageTest(String name) {
+    public JPackageTest(final String name) {
         super(name);
     }
 
@@ -57,12 +60,59 @@ public class JPackageTest extends TestCase {
     }
 
 
+    public void testMatch_perfect_pass() throws Exception {
+
+
+        final JPackage jPackage1;
+        jPackage1 = new JPackage("com.seventytwomiles.project.dao");
+
+        final JPackage jPackage2;
+        jPackage2 = new JPackage("com.seventytwomiles.project.dao");
+
+        assertTrue(jPackage1.equals(jPackage2));
+        assertTrue(jPackage2.equals("com.seventytwomiles.project.dao"));
+
+        assertTrue(jPackage1.equals(jPackage2));
+
+        final List<JPackage> packages = new ArrayList<JPackage>();
+        packages.add(jPackage1);
+        packages.add(jPackage2);
+
+        assertTrue(packages.contains(jPackage1));
+    }
+
+
+    public void testMatch_perfect_fail() throws Exception {
+
+        final JPackage jPackage1;
+        jPackage1 = new JPackage("com.seventytwomiles.project.service");
+
+        final JPackage jPackage2;
+        jPackage2 = new JPackage("com.seventytwomiles.project.dao");
+
+        assertFalse(jPackage1.equals(jPackage2));
+        assertFalse(jPackage2.equals(jPackage1));
+
+
+        assertFalse(jPackage1.equals(new JPackage("")));
+        assertFalse(jPackage1.equals(""));
+
+
+        final List<JPackage> packages = new ArrayList<JPackage>();
+        packages.add(jPackage1);
+
+        assertFalse(packages.contains(jPackage2));
+
+
+    }
+
+
     /**
      * <p>terminating <tt>*</tt> should only look in one package past the last
      * package, not any deeper. so <tt>a.b.*</tt> should match <tt>a.b.c</tt>
      * but NOT <tt>a.b.c.d</tt></p>
      */
-    public void testMatchPackage_pass() throws Exception {
+    public void testMatchPackage_wildcards_pass() throws Exception {
 
 
         final JPackage wild;
@@ -76,7 +126,7 @@ public class JPackageTest extends TestCase {
     }
 
 
-    public void testMatchPackage_fail() throws Exception {
+    public void testMatchPackage_wildcards_fail() throws Exception {
 
         final JPackage wild;
         wild = new JPackage("com.seventytwomiles.project.web.*");
@@ -100,7 +150,7 @@ public class JPackageTest extends TestCase {
      * <tt>a.b.c.d</tt>, <tt>a.b.c.e</tt>, <tt>a.b.c.f</tt> plus
      * <tt>a.b.c.d.1</tt>, <tt>a.b.c.d.2</tt>, <tt>a.b.c.d.3</tt></p>
      */
-    public void testMatchSubPackage_pass() throws Exception {
+    public void testMatchSubPackage_wildcards_pass() throws Exception {
 
         final JPackage wild;
         wild = new JPackage("com.seventytwomiles.project..*");
@@ -118,18 +168,137 @@ public class JPackageTest extends TestCase {
         assertTrue(wild.matches(
                 new JPackage(
                         "com.seventytwomiles.project.dao.hibernate.stub")));
-
     }
 
 
-    public void testMatchSubPackage_fail() throws Exception {
+    public void testMatchSubPackage_wildcards_fail() throws Exception {
 
         final JPackage wild;
         wild = new JPackage("com.seventytwomiles.project2..*");
 
         assertFalse(wild.matches(
                 new JPackage("com.seventytwomiles.project.dao.hibernate")));
-
     }
 
+
+    public void testMatchInnerPackage_wildcards_pass() throws Exception {
+
+        final JPackage wild;
+        wild = new JPackage("com.seventytwomiles.*.dao.hibernate");
+
+        assertTrue(wild.matches(
+                new JPackage("com.seventytwomiles.project1.dao.hibernate")));
+
+        assertTrue(wild.matches(
+                new JPackage(
+                        "com.seventytwomiles.project2.dao.hibernate")));
+
+        assertTrue(wild.matches(
+                new JPackage("com.seventytwomiles.project3.dao.hibernate")));
+    }
+
+
+    public void testMatchInnerPackage_wildcards_fail() throws Exception {
+
+        final JPackage wild;
+        wild = new JPackage("com.seventytwomiles.*.dao.hibernate");
+
+        assertFalse(wild.matches(
+                new JPackage("com.seventytwomiles.project1.services")));
+
+        assertFalse(wild.matches(
+                new JPackage(
+                        "com.seventytwomiles.dao.hibernate")));
+
+        assertFalse(wild.matches(
+                new JPackage("com.seventytwomiles.*.dao.hibernate")));
+    }
+
+
+    public void testMatchInnerAndTerminatingPackage_wildcards_pass()
+            throws Exception {
+
+        final JPackage wild;
+        wild = new JPackage("com.seventytwomiles.*.dao.*");
+
+        assertTrue(wild.matches(
+                new JPackage("com.seventytwomiles.project1.dao.hibernate")));
+
+        assertTrue(wild.matches(
+                new JPackage("com.seventytwomiles.project2.dao.hibernate")));
+
+        assertTrue(wild.matches(
+                new JPackage("com.seventytwomiles.project1.dao.ldap")));
+
+        assertTrue(wild.matches(
+                new JPackage("com.seventytwomiles.project3.dao.jdbc")));
+    }
+
+
+    public void testMatchInnerAndTerminatingPackage_pass()
+            throws Exception {
+
+        assertTrue(new JPackage("com.seventytwomiles..*.filter.*").matches(
+                new JPackage(
+                        "com.seventytwomiles.project1.web.filter.security")));
+
+        assertTrue(new JPackage("com.seventytwomiles..*.filter").matches(
+                new JPackage(
+                        "com.seventytwomiles.project1.web.filter")));
+
+        assertTrue(new JPackage("com.seventytwomiles..*.filter..*").matches(
+                new JPackage(
+                        "com.seventytwomiles.project1.web.filter.security.ws")));
+
+        assertTrue(new JPackage("com.seventytwomiles.*.web.*").matches(
+                new JPackage(
+                        "com.seventytwomiles.project1.web.filter")));
+
+        assertTrue(new JPackage("com.seventytwomiles.*.web..*").matches(
+                new JPackage(
+                        "com.seventytwomiles.project1.web.filter")));
+
+        assertTrue(new JPackage("com.seventytwomiles.*.web..*").matches(
+                new JPackage(
+                        "com.seventytwomiles.project1.web.filter.security")));
+
+        assertTrue(new JPackage("com..*").matches(
+                new JPackage(
+                        "com.seventytwomiles.project1")));
+    }
+
+
+    public void testMatchInnerAndTerminatingPackage_fail()
+            throws Exception {
+
+        assertFalse(new JPackage("com.seventytwomiles..*.filter.*").matches(
+                new JPackage(
+                        "com.seventytwomiles.project1.web.controller.api")));
+
+        assertFalse(new JPackage("com.seventytwomiles..*.filter").matches(
+                new JPackage(
+                        "com.seventytwomiles.filter")));
+
+        assertFalse(new JPackage("com.seventytwomiles..*.filter..*").matches(
+                new JPackage(
+                        "com.seventytwomiles.filter.security.ws")));
+
+        assertFalse(new JPackage("com.seventytwomiles.*.web.*").matches(
+                new JPackage(
+                        "com.seventytwomiles.project1.web")));
+
+        assertFalse(new JPackage("com.seventytwomiles.*.web..*").matches(
+                new JPackage(
+                        "com.seventytwomiles.web.filter")));
+
+        assertFalse(new JPackage("com.seventytwomiles.*.web..*").matches(
+                new JPackage(
+                        "com.seventytwomiles.project1.web")));
+
+        assertFalse(new JPackage("com.*").matches(
+                new JPackage(
+                        "com.seventytwomiles.project1")));
+    }
 }
+
+
