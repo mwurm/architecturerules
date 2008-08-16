41c937b9b6396084d3974372b5e50055351470f5
mikenereson <mikenereson@fa5e1c15-7635-0410-8054-0fc223291177>
2008-08-16 13:40:34 +0000

    optimize imports

--- architecture-rules/src/test/java/test/com/seventytwomiles/services/PersonService.java
+++ architecture-rules/src/test/java/test/com/seventytwomiles/services/PersonService.java
@@ -1,17 +1,16 @@
 /**
  * Copyright 2007, 2008 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *         http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * For more information visit
  *         http://72miles.com/ and
- *         http://architecturerules.googlecode.com/
+ *         http://architecturerules.googlecode.com
  */
-
 package test.com.seventytwomiles.services;
 
 
